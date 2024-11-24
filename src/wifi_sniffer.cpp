91cf16fb7f56fdcd524f3386e43064fa6a99d320
Ilia <dukhov03@mail.ru>
2024-11-24 20:22:47 +0400

    Update wifi_sniffer.cpp

--- src/wifi_sniffer.cpp
+++ src/wifi_sniffer.cpp
@@ -1,11 +1,11 @@
 #include "wifi_sniffer.h"
 
 void scanNetworks(String* ssids, int* deviceCounts, int& networkCount) {
-    int foundNetworks = WiFi.scanNetworks(); // Сканирование доступных сетей
+    int foundNetworks = WiFi.scanNetworks(); 
     networkCount = (foundNetworks > WIFI_CHANNEL_MAX) ? WIFI_CHANNEL_MAX : foundNetworks;
 
     for (int i = 0; i < networkCount; i++) {
-        ssids[i] = WiFi.SSID(i);    // Сохранение SSID
-        deviceCounts[i] = WiFi.RSSI(i); // Сохранение уровня сигнала (RSSI)
+        ssids[i] = WiFi.SSID(i);    
+        deviceCounts[i] = WiFi.RSSI(i); 
     }
 }
