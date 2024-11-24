21f5b8bc06ba92d16864d694b7d939b660fc1862
Ilia <dukhov03@mail.ru>
2024-11-24 20:22:34 +0400

    Update oled_display.h

--- src/oled_display.h
+++ src/oled_display.h
@@ -1,11 +1,11 @@
 #ifndef OLED_DISPLAY_H
 #define OLED_DISPLAY_H
 
-#include <Arduino.h>            // Основная библиотека Arduino
-#include <Wire.h>               // Для работы с шиной I2C
-#include <Adafruit_GFX.h>       // Графическая библиотека Adafruit
-#include <Adafruit_SSD1306.h>   // Библиотека для дисплея SSD1306
-#include "config.h"             // Настройки проекта
+#include <Arduino.h>            
+#include <Wire.h>               
+#include <Adafruit_GFX.h>       
+#include <Adafruit_SSD1306.h>   
+#include "config.h"             
 
 
 void initDisplay();
